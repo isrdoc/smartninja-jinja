2cf64fa58118704b1b9ce13e7856956644c2c8de
Igor Srdoc <igor@srdoc.si>
2020-05-21 18:30:30 +0200

    Add basic template operations

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    title = "Mainpage"
+    time_now = datetime.now()
+    people = ["Janko", "Metka", "Jure"]
+
+    return render_template("index.html", title=title, time_now=time_now, people=people)
 
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    title = "About Jinja"
+    return render_template("about.html", title=title)
 
 
 if __name__ == '__main__':
